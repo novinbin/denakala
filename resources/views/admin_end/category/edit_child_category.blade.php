7fcb53cc6bced14a2034823e2692d518ac3445ab
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-11-27 12:36:51 +0330

    adv category edit fixed updated

--- resources/views/admin_end/category/edit_child_category.blade.php
+++ resources/views/admin_end/category/edit_child_category.blade.php
@@ -1,5 +1,4 @@
 @foreach($child as $category)
-
     @if(count($category->children))
         <div class="accordion item-category mt-2">
             <div class="accordion-item">
@@ -28,13 +27,8 @@
                     </div>
                 </div>
             </div>
-           {{-- <div>{{ $category->title }}</div>
-            <input type="radio" name="advertisement" value="{{ $category->id }}">--}}
         </div>
-
     @endif
-
-
     <div class="collapse" id="collapse{{$category->id}}">
         @if($category->hasChildren())
             @include('admin_end.category.edit_child_category',['child'=>$category->children])
@@ -42,35 +36,7 @@
             <div>{{ __('messages.there_is_no_child') }}</div>
         @endif
     </div>
-
 @endforeach
-{{--@foreach($child as $category)
-
-    <div class="accordion item-category">
-        <div class="accordion-item">
-            <div class="accordion-header item-category-title" id="headingOne">
-                @if($category->hasChildren())
-                    <a class="my-3 btn  accordion-button" href="#collapse{{ $category->id }}" data-bs-toggle="collapse" aria-expanded="true" aria-controls="collapseOne">
-                        <h6>{{ $category->title }}</h6>
-                    </a>
-                @else
-                    <div>{{ __('messages.there_is_no_child') }}</div>
-                @endif
-            </div>
-        </div>
-    </div>
-
-    <div class="collapse" id="collapse{{$category->id}}">
-        @if($category->hasChildren())
-            @include('admin_end.category.child_category',['child'=>$category->children])
-        @else
-            <div>{{ __('messages.there_is_no_child') }}</div>
-        @endif
-    </div>
-
-@endforeach--}}
-
-{{-- <input type="radio" name="advertisement" value="{{ $category->id }}"> --}}
 
 
 
